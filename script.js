d654b89b7b6dfc88932ffcea33b657fc8c30c0ce
NeoBitose <ramadhanalif430@gmail.com>
2024-03-25 19:05:59 +0700

    fix(landing): fixing javascript for event function

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-var content = document.getElementById("content")
-var title_content = document.getElementById("title-content");
-var desc_text = document.getElementById("desc-text");
-var box = document.getElementById("box")
-var desc_img = document.getElementById("desc-img");
-var button_group = document.getElementById("button-group");
+const content = document.getElementById("content")
+const title_content = document.getElementById("title-content");
+const desc_text = document.getElementById("desc-text");
+const box = document.getElementById("box")
+const desc_img = document.getElementById("desc-img");
+const button_group = document.getElementById("button-group");
+const button_true = document.getElementById("button-true");
+const button_false = document.getElementById("button-false");
 
-title_content.innerHTML = "Yakin kan Profesi Anda !";
-desc_text.innerHTML = "Apakah anda seorang Detective ?";
-desc_img.src = "Assets/detektif.svg"
-
-function trueButton(){
+button_true.addEventListener('click', ()=> {
   title_content.innerHTML = "Anda Berbohong!";
   desc_text.innerHTML = "Anda adalah seorang Teknisi";
   box.style.backgroundColor = "#E12E2E";
   desc_img.src = "Assets/teknisi.svg";
   button_group.style.display = 'none';
   content.classList.add("animation-getar");
-}
+});
 
-function falseButton(){
+button_false.addEventListener('click', ()=> {
   title_content.innerHTML = "Anda Benar!";
   desc_text.innerHTML = "Anda adalah seorang Mahasiswa";
   box.style.backgroundColor = "#6FD240"
   desc_img.src = "Assets/mahasiswa.svg";
   button_group.style.display = 'none';
   content.classList.add("animation-naikturun");
-}
+});
