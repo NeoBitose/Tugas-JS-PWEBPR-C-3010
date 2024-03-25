d8c55514de35091a0139c970314cc88464f9c14f
NeoBitose <ramadhanalif430@gmail.com>
2024-03-25 14:31:55 +0700

    feat(lannding): add landing page, css and event function

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var title_content = document.getElementById("title-content");
+var desc_text = document.getElementById("desc-text");
+var box = document.getElementById("box")
+var desc_img = document.getElementById("desc-img");
+var button_group = document.getElementById("button-group");
+
+title_content.innerHTML = "Yakin kan Profesi Anda !";
+desc_text.innerHTML = "Apakah anda seorang Detective ?";
+desc_img.src = "Assets/detektif.svg"
+
+function trueButton(){
+  title_content.innerHTML = "Anda Berbohong!";
+  desc_text.innerHTML = "Anda adalah seorang Teknisi";
+  box.style.backgroundColor = "#E12E2E";
+  desc_img.src = "Assets/teknisi.svg";
+  button_group.style.display = 'none'
+}
+
+function falseButton(){
+  title_content.innerHTML = "Anda Benar!";
+  desc_text.innerHTML = "Anda adalah seorang Mahasiswa";
+  box.style.backgroundColor = "#6FD240"
+  desc_img.src = "Assets/mahasiswa.svg";
+  button_group.style.display = 'none'
+}
